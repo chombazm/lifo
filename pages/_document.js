2f044c9bd38311dd14b4799d856a2506814a3f91
Chomba <dev.chomba@gmail.com>
2023-06-17 14:03:05 +0200

    Initialise project

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        <meta name='description' content='Limitless foundation organisation' />
+        <meta
+          name='keywords'
+          content='Charity, NGO, Non-Profit Organization, Donation, Fundraising, Charity Hub, Charity Hub HTML Template, Charity Hub Template, Charity Hub HTML'
+        />
+
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
